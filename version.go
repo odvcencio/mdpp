e27e27ed7fd63f6e3da138f205885c2217d8775a
Oscar Villavicencio <odvcencio@gmail.com>
2026-04-20 01:41:24 -0700

    bump(version): to 0.1.10
    - Update Version constant from 0.1.1 to 0.1.10

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package mdpp
 
 // Version is the Markdown++ engine version.
-const Version = "0.1.1"
+const Version = "0.1.10"
 
 // SpecVersion is the Markdown++ format version implemented by this package.
 const SpecVersion = "0.1"
