0cf575ee71176240b2574ad04f68af9df33af327
Oscar Villavicencio <odvcencio@gmail.com>
2026-05-30 02:30:41 -0700

    bump(version): bump version to 0.4.4
    - Update minor version from 0.4.3 to 0.4.4

--- version.go
+++ version.go
@@ -1,7 +1,7 @@
 package mdpp
 
 // Version is the Markdown++ engine version.
-const Version = "0.4.3"
+const Version = "0.4.4"
 
 // SpecVersion is the Markdown++ format version implemented by this package.
 const SpecVersion = "0.1"
